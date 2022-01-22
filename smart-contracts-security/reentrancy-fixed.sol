c0ac8208810d2ad3d0da68e0f243d1709e8fc64b
Chris Kag <lekag.eth@gmail.com>
2022-01-23 01:38:48 +0200

    Reentrancy Guard

--- smart-contracts-security/reentrancy-fixed.sol
+++ smart-contracts-security/reentrancy-fixed.sol
@@ -1,6 +1,7 @@
-pragma solidity ^0.8.0;
-
 contract EtherStore {
+
+  // initialize the mutex
+  bool reEntrancyMutex = false
   uint256 public withdrawLimit = 1 ether;
   mapping(address => uint256) public lastWithdrawTime;
   mapping(address => uint256) public balances;
@@ -9,14 +10,19 @@ contract EtherStore {
     balances[msg.sender] += msg.value;
   }
 
-  function withdrawFunds (uint256 _amountToWithdraw) public {
+  function withdrawFunds (uint256 _amountToWithdraw) public
+    require(!reEntrancyMutex);
     require(balances[msg.sender] >= _amountToWithdraw);
     // limit the withdrawal
     require(_amountToWithdraw <= withdrawalLimit);
     // limit the time allowed to withdrawal
     require(now >= lastWithdrawTime[msg.sender] + 1 weeks);
-    require(msg.sender.call.value(_amountToWithdraw)());
     balances[msg.sender] -= _amountToWithdraw;
     lastWithdrawTime[msg.sender] = now;
+    // set the reEntrancy mutex before the external call
+    reEntrancyMutex = true;
+    require(msg.sender.call.value(_amountToWithdraw)());
+    // release the mutex after the external call
+    reEntrancyMutex = false;
   }
 }
